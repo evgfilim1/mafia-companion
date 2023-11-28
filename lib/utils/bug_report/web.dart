a8a31b1dfc05d347eb1410884a7135942dded7a2
evgfilim1 <evgfilim1@yandex.ru>
2023-11-28 14:55:58 +0500

    Download file on web instead of copying bug report contents

--- lib/utils/bug_report/web.dart
+++ lib/utils/bug_report/web.dart
@@ -1,16 +1,16 @@
+import "package:file_saver/file_saver.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 
-import "../errors.dart";
-import "../ui.dart";
 import "stub.dart";
 
 Future<void> reportBug(BuildContext context) async {
   final jsonData = await reportBugCommonImpl(context);
 
-  await Clipboard.setData(ClipboardData(text: "```json\n$jsonData\n```"));
-  if (!context.mounted) {
-    throw ContextNotMountedError();
-  }
-  showSnackBar(context, const SnackBar(content: Text("Отчёт скопирован в буфер обмена")));
+  await FileSaver.instance.saveFile(
+    name: "bug_report",
+    ext: "json",
+    bytes: Uint8List.fromList(jsonData.codeUnits),
+    mimeType: MimeType.json,
+  );
 }
